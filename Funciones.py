0ecc5c48ab6fba488ddb6cbe78b9be2eb819077e
AgustinaDiCarlo <dicarlo.agustina00@gmail.com>
2024-05-23 18:13:41 -0300

    base

---
+++ Funciones.py
@@ -0,0 +1,10 @@
+
+def sumar(a)
+def dividir(a:int,b:int):
+    """Hace una división entre dos números 
+
+    Returns:
+        un float
+    """
+    resultado = a / b
+    return resultado
--- Main.py
+++ Main.py
@@ -1,5 +1,20 @@
-print("abc")
+print("Calculadora iniciada \n")
+print("1. Ingresar 1er operando")
+print("2. Ingresar 2do operando")
+print("3. Calcular todas las operaciones")
+print("4. Informar resultados")
+print("5. Salir")
 
-print("abc")
-print("abc")
-print("abc")
+opcion = input("Elija una opción:\n ")
+
+rta = "s"
+while rta == "s":
+    match opcion:
+        case "1":
+            primerValor = input("Ingrese el primer valor")
+        case "2":
+            segundoValor = input("Ingrese el segundo valor")
+        case "3":
+            
+        case "4":
+        case "5":
